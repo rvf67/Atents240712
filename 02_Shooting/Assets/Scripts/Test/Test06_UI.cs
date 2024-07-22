c2b59399245e1d3c4652b5114a1f860f3516210e
rvf67 <62585538+rvf67@users.noreply.github.com>
2024-07-22 13:52:20 +0900

    UI 주석

--- 02_Shooting/Assets/Scripts/Test/Test06_UI.cs
+++ 02_Shooting/Assets/Scripts/Test/Test06_UI.cs
@@ -15,11 +15,11 @@ public class Test06_UI : TestBase
         //GameObject.FindWithTag;
 
         //컴포넌트 타입으로 찾음
-        ScoreText scoreText= FindObjectOfType<ScoreText>();//하나만 찾기
-        ScoreText[] scoreTexts = FindObjectsByType<ScoreText>
-            (FindObjectsInactive.Include, FindObjectsSortMode.None);//같은 컴포넌트 여러개 찾기
-        FindAnyObjectByType<ScoreText>();//하나만 찾기 (첫번째보나 빠름
-        FindFirstObjectByType<ScoreText>();//첫번째 찾기 (속도는 느림 ,순서가 중요
+        //ScoreText scoreText= FindObjectOfType<ScoreText>();//하나만 찾기
+        //ScoreText[] scoreTexts = FindObjectsByType<ScoreText>
+        //    (FindObjectsInactive.Include, FindObjectsSortMode.None);//같은 컴포넌트 여러개 찾기
+        //FindAnyObjectByType<ScoreText>();//하나만 찾기 (첫번째보나 빠름
+        //FindFirstObjectByType<ScoreText>();//첫번째 찾기 (속도는 느림 ,순서가 중요
         
     }
     int Add(int a, int b)
